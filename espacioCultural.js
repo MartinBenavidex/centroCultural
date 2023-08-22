d97029a0697e85425b5fb30b98bf78b38c54d070
MartinBenavidex <137458839+MartinBenavidex@users.noreply.github.com>
2023-08-22 02:18:44 -0300

    Add files via upload

--- espacioCultural.js
+++ espacioCultural.js
@@ -1,4 +1,16 @@
-        const eventosCulturales = [];
+const d = document 
+const cuerpo = d.body
+const cabeza = d.head
+const titulo = d.createElement('title')
+const cabecera = d.getElementById('header')
+const banner = titulo.innerText = 'El Espacio Cultural'
+cabeza.appendChild(titulo)
+const fotter = d.querySelector('#p-footer');
+const anio = new Date().getFullYear();
+fotter.innerHTML = `<a href="https://www.instagram.com/centroculturalelespaciook/"_blank">&copy | ${banner} | ${anio}</a>`
+//const eventosCulturales = [];
+
+const eventosCulturales = JSON.parse(localStorage.getItem('eventos')) || [];
 
         function agregarEvento() {
             const nombre = document.getElementById("nombre").value;
@@ -12,99 +24,97 @@
             };
 
             eventosCulturales.push(evento);
+            localStorage.setItem('eventos', JSON.stringify(eventosCulturales));
 
             alert(`¡El evento "${nombre}" ha sido agregado correctamente!`);
-            actualizarEventosDropdown();
         }
 
         function mostrarEventos() {
             const listaEventos = document.getElementById("listaEventos");
             listaEventos.innerHTML = "";
 
-            if (eventosCulturales.length === 0) {
-                alert("No hay eventos disponibles en este momento.");
-            } else {
-                eventosCulturales.forEach((evento, index) => {
-                    const li = document.createElement("li");
-                    li.textContent = `${index + 1}. ${evento.nombre} - ${evento.fecha}`;
-                    listaEventos.appendChild(li);
-                });
-            }
-        }
-
-        function actualizarEventosDropdown() {
-            const eventoSeleccionado = document.getElementById("eventoSeleccionado");
-            eventoSeleccionado.innerHTML = "";
-
             eventosCulturales.forEach((evento, index) => {
-                const option = document.createElement("option");
-                option.value = index;
-                option.textContent = evento.nombre;
-                eventoSeleccionado.appendChild(option);
+                const li = document.createElement("li");
+                li.textContent = `${index + 1}. ${evento.nombre} - ${evento.fecha}`;
+                listaEventos.appendChild(li);
             });
         }
 
-        function agregarAlCarrito() {
-            const eventoSeleccionado = document.getElementById("eventoSeleccionado").value;
-            const totalCarritoElement = document.getElementById("totalCarrito");
+        mostrarEventos();
 
-            if (eventoSeleccionado !== "") {
-                totalCarrito += 1000; // Precio de la entrada en pesos argentinos
-                totalCarritoElement.textContent = totalCarrito;
+function actualizarEventosDropdown() {
+    const eventoSeleccionado = document.getElementById("eventoSeleccionado");
+    eventoSeleccionado.innerHTML = "";
+
+    eventosCulturales.forEach((evento, index) => {
+        const option = document.createElement("option");
+        option.value = index;
+        option.textContent = evento.nombre;
+        eventoSeleccionado.appendChild(option);
+    });
+}
+
+function agregarAlCarrito() {
+    const eventoSeleccionado = document.getElementById("eventoSeleccionado").value;
+    const totalCarritoElement = document.getElementById("totalCarrito");
+
+    if (eventoSeleccionado !== "") {
+        totalCarrito += 1000; // Precio de la entrada en pesos argentinos
+        totalCarritoElement.textContent = totalCarrito;
+        Swal.fire({
+            icon: 'success',
+            title: 'Entrada sumada al carrito',
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+}
+function abonarReservas() {
+    if (totalCarrito > 0) {
+        Swal.fire({
+            title: 'Abonar Reservas',
+            text: `El total a abonar es: ${totalCarrito} pesos argentinos`,
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonText: 'Confirmar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
                 Swal.fire({
                     icon: 'success',
-                    title: 'Entrada sumada al carrito',
+                    title: 'Reservas abonadas correctamente',
                     showConfirmButton: false,
                     timer: 1500
                 });
+
+                totalCarrito = 0;
+                document.getElementById("totalCarrito").textContent = totalCarrito;
             }
-        }
-        function abonarReservas() {
-            if (totalCarrito > 0) {
-                Swal.fire({
-                    title: 'Abonar Reservas',
-                    text: `El total a abonar es: ${totalCarrito} pesos argentinos`,
-                    icon: 'info',
-                    showCancelButton: true,
-                    confirmButtonText: 'Confirmar',
-                    cancelButtonText: 'Cancelar'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Reservas abonadas correctamente',
-                            showConfirmButton: false,
-                            timer: 1500
-                        });
-
-                        totalCarrito = 0;
-                        document.getElementById("totalCarrito").textContent = totalCarrito;
-                    }
-                });
-            } else {
-                Swal.fire({
-                    icon: 'warning',
-                    title: 'El carrito está vacío',
-                    showConfirmButton: false,
-                    timer: 1500
-                });
-            }
-        }
+        });
+    } else {
+        Swal.fire({
+            icon: 'warning',
+            title: 'El carrito está vacío',
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+}
 
 
-        let darkMode = false;
+let darkMode = false;
 
-        function alternarDarkMode() {
-            darkMode = !darkMode;
-            cambiarColorFondo();
-        }
+function alternarDarkMode() {
+    darkMode = !darkMode;
+    cambiarColorFondo();
+}
 
-        function cambiarColorFondo() {
-            const body = document.body;
-            if (darkMode) {
-                body.style.backgroundColor = "darkgray";
-            } else {
-                body.style.backgroundColor = "#f9f9f9";
-            }
-        }
-        let totalCarrito = 0;
+function cambiarColorFondo() {
+    const body = document.body;
+    if (darkMode) {
+        body.style.backgroundColor = "darkgray";
+    } else {
+        body.style.backgroundColor = "#f9f9f9";
+    }
+}
+let totalCarrito = 0;
